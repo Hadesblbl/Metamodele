62f714235ac2d65a774b821d2c929e1d74ab66c9
Thomas Blangero <chungaishaeve@gmail.com>
2018-02-21 09:59:00 +0100

    last version

--- MetaModel/MetaModel/UnaryExpressionModel.h
+++ MetaModel/MetaModel/UnaryExpressionModel.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef UNARYEXPRESSIONMODEL_H
+#define UNARYEXPRESSIONMODEL_H
+#include "UnaryExpression.h";
+
+using namespace core;
+
+template <class T>;
+class UnaryExpressionModel : public UnaryExpression {
+};
+
+T UnaryExpressionModel::UnaryExpression:evaluate(Expression o) const {
+	if (operator!=null) {
+		return operator.evaluate(o);
+	}
+}
+
+T UnaryExpressionModel::Expression:evaluate() const {
+	if (operand != null) {
+		return evaluate(operand);
+	}
+}
+#endif // !UNARYEXPRESSIONMODEL_H
