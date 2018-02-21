fb3dd80176b99f10d45eed1b72bbf82871adc198
Bastien Geldreich <bastien.geldreich@free.fr>
2018-02-21 09:20:44 +0100

    debut

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Metamodele
+//
+//  Created by Bastien Geldreich on 21/02/2018.
+//  Copyright © 2018 Bastien Geldreich. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
