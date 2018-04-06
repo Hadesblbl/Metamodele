72bf6c83fc313f57bead25e7e48dafacbd001784
Thomas Blangero <chungaishaeve@gmail.com>
2018-04-06 08:56:09 +0200

    maj

--- MetaModel/MetaModel/NaryExpression.h
+++ MetaModel/MetaModel/NaryExpression.h
@@ -6,7 +6,11 @@ namespace core {
 	template <class T>
 	class NaryExpression abstract {
 	public:
+<<<<<<< HEAD
 		virtual T evaluate(Expression**) const;
+=======
+		virtual T evaluate(Expression<T>[] o) const;
+>>>>>>> 6680505190c9798b1ec08b3bcfaf05ab963a2e61
 	};
 }
 #endif
