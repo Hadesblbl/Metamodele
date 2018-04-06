6680505190c9798b1ec08b3bcfaf05ab963a2e61
Bastien Geldreich <bastien.geldreich@free.fr>
2018-04-06 08:40:40 +0200

    evaluate sugeno

--- MetaModel/MetaModel/SugenoConclusion.h
+++ MetaModel/MetaModel/SugenoConclusion.h
@@ -9,4 +9,12 @@ class SugenoDefuzz : public NaryExpression{
         T* coeff;
         T evaluate(Expression<T>**);
 }
+
+public T SugenoDefuzz::evaluate(Expressions<T>** operands){
+    T result=0;
+    for(int i=0;i<strlen(coeff);i++){
+        result+=coeff[i] * operands[i];
+    }
+    return result;
+}
 #endif
