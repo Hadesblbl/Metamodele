e9c6110b260e611a3c9396202570c0f0ba1c8954
kaelhanas <gkylian@gmail.com>
2018-06-02 22:19:46 +0200

    modif Agg AggMax AggPlus

--- MetaModel/MetaModel/AggMax.h
+++ MetaModel/MetaModel/AggMax.h
@@ -1,22 +1,27 @@
 #ifndef AggMax_h
 #define AggMax_h
+
 #include "BinaryExpression.h";
 #include "Agg.h";
 
 using namespace core;
-template <class T>
-class AggMax : public Agg<T> {
+
+namespace fuzzy
+{
+	template <class T>
+	class AggMax : public Agg<T> {
 	public:
-		AggMax();
-		~AggMax();
+		AggMax() {};
+		virtual ~AggMax() {};
 		T evaluate(Expression<T> *, Expression<T> *) const;
-};
+	};
 
-template <class T>
-T AggMax<T>::evaluate(Expression<T> *l, Expression<T> *r) const {
-	T left = l->evaluate();
-	T right = r->evaluate();
-	return std::max(left,right);
+	template <class T>
+		T AggMax<T>::evaluate(Expression<T> *l, Expression<T> *r) const {
+		T left = l->evaluate();
+		T right = r->evaluate();
+		return std::max(left, right);
+	}
+
 }
-
 #endif /* AggMax_h */
--- MetaModel/MetaModel/AggPlus.h
+++ MetaModel/MetaModel/AggPlus.h
@@ -1,23 +1,27 @@
 #ifndef AggPlus_h
 #define AggPlus_h
+
 #include "BinaryExpression.h";
 #include "Agg.h";
 #include "Expression.h"
 
 using namespace core;
-template <class T>
-class AggPlus : public Agg<T> {
+
+namespace fuzzy
+{
+	template <class T>
+	class AggPlus : public Agg<T> {
 	public:
-		AggPlus();
-		~AggPlus();
+		AggPlus() {};
+		virtual ~AggPlus();
 		T evaluate(Expression<T> *, Expression<T> *) const;
-};
+	};
 
-template <class T>
-T AggPlus<T>::evaluate(Expression<T> *l, Expression<T> *r) const {
-	T left = l->evaluate();
-	T right = r->evaluate();
-	return left + right;
+	template <class T>
+		T AggPlus<T>::evaluate(Expression<T> *l, Expression<T> *r) const {
+		T left = l->evaluate();
+		T right = r->evaluate();
+		return left + right;
+	};
 }
-
 #endif /* AggPlus_h */
